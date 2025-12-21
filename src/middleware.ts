f02653981742d7742447f80b1e5f8e1f6c78e5df
pratheepyoocode <your_email_used_on_github>
2025-12-21 13:53:40 +0530

    fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,9 +8,12 @@ export async function middleware(request: NextRequest) {
         },
     })
 
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? 'https://mtsmdeyxvsgxazgqbikm.supabase.co'
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10c21kZXl4dnNneGF6Z3FiaWttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4NzU2MzQsImV4cCI6MjA4MTQ1MTYzNH0.Q-fUIsu7x9KSVCMxuqk39fw10Qc_rNpPp315GvMTgxw'
+
     const supabase = createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        supabaseUrl,
+        supabaseAnonKey,
         {
             cookies: {
                 get(name: string) {
