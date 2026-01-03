700f76be84d239951820dd27f2460b5f7985fc6e
pratheepyoocode <your_email_used_on_github>
2026-01-03 20:12:20 +0530

    update razorpay live keys

--- src/utils/razorpay.ts
+++ src/utils/razorpay.ts
@@ -1,6 +1,6 @@
 import Razorpay from 'razorpay';
 
 export const razorpay = new Razorpay({
-    key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID || 'rzp_test_RtwhZZFSvgR3el',
-    key_secret: process.env.RAZORPAY_KEY_SECRET || 'TjHL4rwHo1jV4PlB791LVNUt',
+    key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID || 'rzp_live_RzRKslVD0sIZI0',
+    key_secret: process.env.RAZORPAY_KEY_SECRET || 'B9xHyueoUpP9d84XXGbbNpIk',
 });
